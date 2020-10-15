ccce2b5a93d921400f5817851c623d51d3e68461
Yassssssssss <yassvanth_p@yahoo.com>
2020-10-15 18:25:04 +0100

    fixed a few deck problems

--- src/Deck.java
+++ src/Deck.java
@@ -2,9 +2,9 @@ import java.util.ArrayList
 
 
 class Deck{
-    private ArrayList<int> cards = new ArrayList<int>();
+    private ArrayList<Integer> cards = new ArrayList<Integer>();
 
-    public ArrayList getCards(){
+    public ArrayList<Integer> getCards(){
         return this.cards;
     }
 
