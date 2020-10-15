1a00d5ee8306a5957bd076a085f9fe9bda0b7658
Yassssssssss <yassvanth_p@yahoo.com>
2020-10-15 18:56:38 +0100

    Generate pack and inputNumPlayers + getter

--- src/src/CardGame.java
+++ src/src/CardGame.java
@@ -1,8 +1,19 @@
 package src;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
+
 
 class CardGame {
+    private int numPlayers;
+
+    public CardGame(){
+        this.numPlayers = inputNumPlayers();
+    }
+    public int getNumPlayers(){
+        return this.numPlayers;
+    }
     private Integer inputNumPlayers() {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many players");
@@ -10,10 +21,20 @@ class CardGame {
         sc.close();
         return numPlayers;
     }
-
+    private ArrayList<Integer> generatePack(){
+        ArrayList<Integer> packList = new ArrayList<Integer>();
+        Random r = new Random();
+        for (int i= 0; i < this.numPlayers*8; i++){
+            int x = r.nextInt(this.numPlayers)+1 ;
+            packList.add(x);
+        }
+        return packList;
+    }
     public static void main (String[] args) {
         CardGame cardGame = new CardGame();
-        int numPlayers = cardGame.inputNumPlayers();
+        
+        System.out.println(cardGame.generatePack());
         
     }
+    
 }
