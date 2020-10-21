6a8a6fa10e10a6927893ebc0298fb5c096fc3e58
Yassssssssss <yassvanth_p@yahoo.com>
2020-10-21 20:40:44 +0100

    +inputFile()

--- src/src/CardGame.java
+++ src/src/CardGame.java
@@ -3,27 +3,42 @@ package src;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
-
+import java.io.File;
+import java.io.FileNotFoundException;
 
 class CardGame {
     private int numPlayers;
     private ArrayList<GameObject> gameRing;
+    private ArrayList<Integer> cardList;
+    private Scanner sc;
 
     public CardGame(){
+        this.sc = new Scanner(System.in);
         this.numPlayers = inputNumPlayers();
         this.gameRing = generateRing();
+        this.cardList = readFile(inputCardsFile());
+        this.sc.close();
     }
+
     public int getNumPlayers(){
         return this.numPlayers;
     }
+
     private Integer inputNumPlayers() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("How many players");
         Integer numPlayers = sc.nextInt();
-        sc.close();
+        
         return numPlayers;
     }
-    
+
+    private String inputCardsFile() {
+        System.out.println("Give file name");
+
+        String fileName = sc.next();
+        
+        return fileName;
+    }
+
     private ArrayList<GameObject> generateRing(){
         ArrayList<GameObject> ring = new ArrayList<GameObject>();
         for(int i=0; i< this.numPlayers; i++){
@@ -35,6 +50,20 @@ class CardGame {
         return ring;
     }
 
+    private ArrayList<Integer> readFile(String fileName){
+        ArrayList<Integer> returnPack = new ArrayList<Integer>();
+        try {
+            File file = new File(fileName);
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                returnPack.add(Integer.parseInt(sc.nextLine()));
+            }
+            sc.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        return returnPack;
+    } 
 
     private ArrayList<Integer> generatePack(){
         ArrayList<Integer> packList = new ArrayList<Integer>();
@@ -45,11 +74,9 @@ class CardGame {
         }
         return packList;
     }
+
     public static void main (String[] args) {
         CardGame cardGame = new CardGame();
         
-        System.out.println(cardGame.generatePack());
-        System.out.println(cardGame.gameRing);
     }
-    
 }
