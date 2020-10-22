8cfad43c40767a1807204eb74105dc3d66debb53
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-22 18:24:10 +0100

    test test

---
+++ src/src/TestGameObject.java
@@ -0,0 +1,14 @@
+package src;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class TestGameObject {
+    public void testAddCard() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("here");
+    }
+}
