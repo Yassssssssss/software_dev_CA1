69b8e981845d3a968c40518918d32ed3a994032b
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-23 18:32:18 +0100

    test gameObject

--- src/src/TestGameObject.java
+++ src/src/TestGameObject.java
@@ -1,13 +1,26 @@
 package src;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 
 public class TestGameObject {
+    GameObject obj;
+
+    @Before
+    public void gameObjectSetup() {
+        this.obj = new GameObject();
+    }
 
     @Test
-    public void testAddCard() {
-
+    public void testGetCard() {
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        for (int i=0; i < 10; i++) expected.add(i);
+        assertEquals(expected, this.obj.getCards());
     }
 
     public static void main(String[] args) {
---
+++ src/src/TestSuite.java
@@ -0,0 +1,11 @@
+package src;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestGameObject.class})
+public class TestSuite {
+    
+}
