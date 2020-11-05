318062b7cca0d5f630626859e61e142283fdd105
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-11-05 18:52:08 +0000

    remove import

--- src/src/CardGame.java
+++ src/src/CardGame.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Set;
 
 //TODO :: 
 //        write to individual txt files 
