2662104b98e66bdd7a840167471f47c69003b354
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-11-08 19:18:58 +0000

    fix bug

--- src/src/InvalidLengthException.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class InvalidLengthException extends Exception {
-    private static final long serialVersionUID = 8174739145878820656L;
-
-    public InvalidLengthException(String message) {
-        super(message);
-    }
-}
